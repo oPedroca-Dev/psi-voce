6066addbf33a7d7082989e0d74cc37d4d24671ea
oPedroca-Dev <carvalhoph26@gmail.com>
2026-01-29 16:16:13 -0300

    .

--- src/components/dep/index.jsx
+++ src/components/dep/index.jsx
@@ -54,7 +54,7 @@ export default function TestimonialsCards() {
 
         <div className="t-cards__action">
           <p>Venha escrever seu próximo capítulo conosco.</p>
-          <a href="https://wa.me/551146733583" className="t-cards__btn">
+          <a href="https://wa.me/551146733583?text=Olá,%20Visitei%20o%20site%20do%20Instituto%20Psi%20%26%20Você%20e%20gostaria%20de%20mais%20informações%20sobre%20o%20atendimento." className="t-cards__btn">
             Agendar Atendimento <FiArrowRight />
           </a>
         </div>
