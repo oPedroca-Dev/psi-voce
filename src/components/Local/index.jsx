918de1843eaac32ea7ab2ae23ff2a302c8a97064
oPedroca-Dev <carvalhoph26@gmail.com>
2026-02-02 18:04:22 -0300

    .

--- src/components/Local/index.jsx
+++ src/components/Local/index.jsx
@@ -1,9 +1,7 @@
-// Location/index.jsx
 import './index.scss';
 import { FiMapPin, FiClock, FiPhone, FiArrowUpRight } from 'react-icons/fi';
 
 export default function Location() {
-  // Substitua pela URL real de incorporação do Google Maps
   const googleMapsIframe = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.4533!2d-46.6853!3d-23.6425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce50f383!2sAv.%20Nossa%20Sra.%20do%20Sabar%C3%A1%2C%201693!5e0!3m2!1spt-BR!2sbr!4v1700000000000";
 
   return (
